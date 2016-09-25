a639c8adff0d02dcaaee014013c9593fbef952c9
Cristopher Chavez <cristopherchavezny@gmail.com>
2016-09-24 23:58:03 -0400

    UILab03 Done

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var cornerLabels: UILabel!
+    
+    @IBOutlet weak var cornerLabel: UILabel!
+    
+    @IBOutlet weak var centerLabelValue: UILabel!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -20,6 +28,29 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
+    @IBAction func clubsFour(_ sender: AnyObject) {
+        cornerLabels.text = "♣️"
+        cornerLabel.text = "♣️"
+        centerLabelValue.text = "4"
+    }
   
+
+    @IBAction func spadesThree(_ sender: AnyObject) {
+        cornerLabels.text = "♠️"
+        cornerLabel.text = "♠️"
+        centerLabelValue.text = "3"
+    }
+        
+    @IBAction func diamondsEight(_ sender: AnyObject) {
+        cornerLabels.text = "♦️"
+        cornerLabel.text = "♦️"
+        centerLabelValue.text = "8"
+    }
+    
+    @IBAction func heartsTen(_ sender: AnyObject) {
+        cornerLabels.text = "♥️"
+        cornerLabel.text = "♥️"
+        centerLabelValue.text = "10"
+    }
 
 }
