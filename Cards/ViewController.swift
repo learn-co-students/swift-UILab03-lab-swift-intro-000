3779acc4b0dce588cbe1f0e4dbb38cec7347df85
briecoyle <brie.coyle@gmail.com>
2016-10-10 10:04:31 -0400

    Thread 1: signal SIGABRT at class AppDelegate line 12 in AppDelegate.swift

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
+    @IBOutlet weak var centerLabel: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,27 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
+    @IBAction func fourOfClubs(_ sender: AnyObject) {
+        leftLabel.text = "♣️"
+        rightLabel.text = "♣️"
+        centerLabel.text = "4"
+    }
+    @IBAction func threeOfSpades(_ sender: AnyObject) {
+        leftLabel.text = "♠️"
+        rightLabel.text = "♠️"
+        centerLabel.text = "3"
+    }
+    @IBAction func eightOfDiamonds(_ sender: AnyObject) {
+        leftLabel.text = "♦️"
+        rightLabel.text = "♦️"
+        centerLabel.text = "8"
+    }
+    @IBAction func tenOfHearts(_ sender: AnyObject) {
+        leftLabel.text = "♥️"
+        rightLabel.text = "♥️"
+        centerLabel.text = "10"
+    }
+    
   
 
 }
