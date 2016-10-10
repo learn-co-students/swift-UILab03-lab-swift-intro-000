24f26c77008738b531861abf2b7b44a3b082f0ae
Jacob Frawley <frawjm12@gmail.com>
2016-10-10 18:31:38 -0400

    Ace of Spades

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var suitOne: UILabel!
+    @IBOutlet weak var suitTwo: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,31 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
+    @IBAction func suitNumb(_ sender: AnyObject) {
+        switch (sender.tag) {
+        case 1:
+            number.text = "4"
+            suitOne.text = "♣️"
+            suitTwo.text = "♣️"
+        case 2:
+            number.text = "3"
+            suitOne.text = "♠️"
+            suitTwo.text = "♠️"
+        case 3:
+            number.text = "8"
+            suitOne.text = "♦️"
+            suitTwo.text = "♦️"
+        case 4:
+            number.text = "10"
+            suitOne.text = "♥️"
+            suitTwo.text = "♥️"
+        default:
+            number.text = "?"
+            suitOne.text = "?"
+            suitTwo.text = "?"
+        }
+        }
+    }
   
 
-}
+
