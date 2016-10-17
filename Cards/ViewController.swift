5b8f3004e73f4356fb78471a6421355b451eb443
ac <ac@Jaxxs-MBP.home>
2016-10-17 18:34:40 -0400

    completed assignment

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,14 +12,46 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var midLabel: UILabel!
+    @IBOutlet weak var btmLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        topLabel.text = "?"
+        midLabel.text = "?"
+        btmLabel.text = "?"
     }
     
     
     
     // TODO: IB actions and code to update UI
-
+    
+    @IBAction func club4(_ sender: AnyObject) {
+        topLabel.text = "♣️"
+        btmLabel.text = "♣️"
+        midLabel.text = "4"
+    }
+    
+    @IBAction func spade3(_ sender: AnyObject) {
+        topLabel.text = "♠️"
+        btmLabel.text = "♠️"
+        midLabel.text = "3"
+        
+    }
   
+    @IBAction func diamond8(_ sender: AnyObject) {
+        topLabel.text = "♦️"
+        btmLabel.text = "♦️"
+        midLabel.text = "8"
+    }
 
+    @IBAction func heart10(_ sender: AnyObject) {
+        topLabel.text = "♥️"
+        btmLabel.text = "♥️"
+        midLabel.text = "10"
+    }
+    
+    
 }
