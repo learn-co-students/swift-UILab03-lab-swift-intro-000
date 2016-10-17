b28fe4144cb1931692e86416cb7009222a06a2e3
jakim54 <james.kim@huffingtonpost.com>
2016-10-17 14:45:23 -0400

    wrote todo, thought of possible solutions in playground

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -23,3 +23,12 @@ class ViewController: UIViewController {
   
 
 }
+
+/*
+ TODO:
+ - Create a label in the top left and bottom right corner of the app view. You might want to bump the text size up to 28pt. (Don't forget about the Attributes Inspector!)
+ - Add a label to the center of the view. Its text size should be 40 pt.
+ - Create four buttons with the follow labels: 4 of clubs, 3 of spades, 8 of diamonds, and 10 of hearts
+ - Hook these buttons up to IBActions. You can hook them each up to their own IBActions, or just have one single IBActions that they are all attached to.
+ - When a button is pressed, update the labels accordingly (the ones in the corner should display the suit and the one in the center should display the value).
+ */
