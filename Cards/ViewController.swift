e4fdc53226a626af0ea188221f5c063be7bd8da4
hingyenyin <yenyin.262@gmail.com>
2016-11-07 06:18:17 +0800

    ish

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var rightCorner: UILabel!
+    
+    @IBOutlet weak var middleValue: UILabel!
+    
+    @IBOutlet weak var leftCorner: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -20,6 +26,51 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
-  
+
+   
+    @IBAction func selectedCards(_ sender: UIButton) {
+   
+        let selected = sender.currentTitle
+        
+        switch selected!{
+       
+        case "♣️4":
+        
+            rightCorner.text = "♣️"
+            leftCorner.text = "♣️"
+            middleValue.text = "4"
+        
+        case "♠️3":
+            
+            rightCorner.text = "♠️"
+            leftCorner.text = "♠️"
+            middleValue.text = "3"
+            
+        case "♦️8":
+            
+            rightCorner.text = "♦️"
+            leftCorner.text = "♦️"
+            middleValue.text = "8"
+            
+            
+        case "♥️10":
+            
+            rightCorner.text = "♥️"
+            leftCorner.text = "♥️"
+            middleValue.text = "10"
+            
+            
+        default:
+            
+            break
+        
+            
+    
+        }
+    
+    
+    
+    
+    }
 
 }
