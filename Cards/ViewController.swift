e0ba255b2bf17b163c0f16d437330def962b6ec4
JJJV <juan@jaramillo.net>
2016-12-27 01:59:09 -0600

    JJ Solution

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,11 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+        
     // TODO: Create IB outlets
+    
+    @IBOutlet weak var topCorner: UILabel!
+    
+    @IBOutlet weak var bottomCorner: UILabel!
+    
+    @IBOutlet weak var centerNumber: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        topCorner.text = ""
+        bottomCorner.text = ""
+        centerNumber.text = ""
+        
+        
     }
+    
+    
+    
+    @IBAction func buttonOne(_ sender: AnyObject) {
+        
+        topCorner.text = "♣️"
+        bottomCorner.text = "♣️"
+        centerNumber.text = "4"
+    }
+    
+    @IBAction func buttonTwo(_ sender: AnyObject) {
+        topCorner.text = "♠️"
+        bottomCorner.text = "♠️"
+        centerNumber.text = "3"
+        
+    }
+    
+    @IBAction func buttonThree(_ sender: AnyObject) {
+        topCorner.text = "♦️"
+        bottomCorner.text = "♦️"
+        centerNumber.text = "8"
+        
+    }
+    
+    
+    @IBAction func buttonFour(_ sender: AnyObject) {
+        
+        topCorner.text = "♥️"
+        bottomCorner.text = "♥️"
+        centerNumber.text = "10"
+        
+    }
+    
+    
+    
+   
     
     
     
