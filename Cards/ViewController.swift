7a30a0361cd767e9bcfa1b93262bf5bf1ddfc4e3
Terrence <Terrence@Terrences-iMac.local>
2017-01-21 03:13:26 -0500

    clockWork

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topRightLabel: UILabel!
+    @IBOutlet weak var bottomLeftLabel: UILabel!
+    @IBOutlet weak var middleLabel: UILabel!
+    @IBOutlet weak var button4Ofclubs: UIButton!
+    @IBOutlet weak var button3OfSpades: UIButton!
+    @IBOutlet weak var button8OfDiamonds: UIButton!
+    @IBOutlet weak var button10OfHearts: UIButton!
+    
     // TODO: Create IB outlets
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func changeToClub(_ sender: UIButton){
+        topRightLabel.text = "♣️"
+        bottomLeftLabel.text = "♣️"
+        middleLabel.text = "4"
+    }
     
-    
+    @IBAction func changeToSpade(_ sender: UIButton){
+        topRightLabel.text = "♠️"
+        bottomLeftLabel.text = "♠️"
+        middleLabel.text = "3"
+    }
+    @IBAction func changeToDiamond(_ sender: UIButton){
+        topRightLabel.text = "♦️"
+        bottomLeftLabel.text = "♦️"
+        middleLabel.text = "8"
+    }
+    @IBAction func changeToHearts(_ sender: UIButton){
+        topRightLabel.text = "❤️"
+        bottomLeftLabel.text = "❤️"
+        middleLabel.text = "10"
+    }
     // TODO: IB actions and code to update UI
-
-  
-
 }
