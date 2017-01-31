f31bc488f9c10e0c067146d0f028df2192efba63
jaugeni <jaugeni@gmail.com>
2017-01-31 18:19:12 -0500

    Complete problems

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // TODO: Create IB outlets
+    
+    @IBOutlet weak var leftTopCornerLbl: UILabel!
+    @IBOutlet weak var rightBottomCornerLbl: UILabel!
+    @IBOutlet weak var centerLbl: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
+    func changeLbl(suit: String, number: String) {
+        leftTopCornerLbl.text = suit
+        rightBottomCornerLbl.text = suit
+        centerLbl.text = number
+    }
     
     // TODO: IB actions and code to update UI
-
-  
-
+    
+    @IBAction func clubsPressed(_ sender: Any) {
+        changeLbl(suit: "♣️", number: "4")
+    }
+    
+    @IBAction func spadesPressed(_ sender: Any) {
+        changeLbl(suit: "♠️", number: "3")
+    }
+    
+    @IBAction func diamondsPressed(_ sender: Any) {
+        changeLbl(suit: "♦️", number: "8")
+    }
+    
+    @IBAction func heardsPressed(_ sender: Any) {
+        changeLbl(suit: "♥️", number: "10")
+    }
+    
 }
