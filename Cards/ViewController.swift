eecf57c4250ec05d6f0a58b662568e7a29c08037
larenSakota <jedi.temple@yahoo.com>
2017-02-07 13:18:55 -0800

    complete

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var cornerSuit: UILabel!
+    @IBOutlet weak var clubs: UIButton!
+    @IBOutlet weak var spades: UIButton!
+    @IBOutlet weak var diamonds: UIButton!
+    @IBOutlet weak var hearts: UIButton!
+    
+    
+    @IBOutlet weak var cornerSuitTop: UILabel!
+
+    @IBOutlet weak var cornerSuitBottom: UILabel!
+    
     @IBOutlet weak var centerNumber: UILabel!
     
     
@@ -18,10 +27,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+
     
-    
-    @IBAction func numberBtn(_ sender: Any) {
+    @IBAction func numberBtn(_ sender: UIButton) {
         
+ 
+        switch sender {
+        case clubs:
+            cornerSuitTop.text = "♣️"
+            cornerSuitBottom.text = "♣️"
+            centerNumber.text = "4"
+        case spades:
+            cornerSuitTop.text = "♠️"
+            cornerSuitBottom.text = "♠️"
+            centerNumber.text = "3"
+        case diamonds:
+            cornerSuitTop.text = "♦️"
+            cornerSuitBottom.text = "♦️"
+            centerNumber.text = "8"
+        case hearts:
+            cornerSuitTop.text = "♥️"
+            cornerSuitBottom.text = "♥️"
+            centerNumber.text = "10"
+
+        default:
+            cornerSuitTop.text = "♥️"
+            cornerSuitBottom.text = "♥️"
+            centerNumber.text = "1"
+        }
         
         
         
