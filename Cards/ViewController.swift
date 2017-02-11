76c7fff53a4df5ad25434a42496dc3c9cac281af
Colin Jao <jao.colin@gmail.com>
2017-02-10 18:02:34 -0600

    Finished Card UI lab

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,8 +12,37 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var topSuit: UILabel!
+    @IBOutlet weak var cardValue: UILabel!
+    @IBOutlet weak var bottomSuit: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func changeCard(_ sender: UIButton) {
+        switch sender.titleLabel?.text{
+        case "♣️4"?:
+            topSuit.text = "♣️"
+            cardValue.text = "4"
+            bottomSuit.text = "♣️"
+        case "♠️3"?:
+            topSuit.text = "♠️"
+            cardValue.text = "3"
+            bottomSuit.text = "♠️"
+        case "♦️8"?:
+            topSuit.text = "♦️"
+            cardValue.text = "8"
+            bottomSuit.text = "♦️"
+        case "♥️10"?:
+            topSuit.text = "♥️"
+            cardValue.text = "10"
+            bottomSuit.text = "♥️"
+        default:
+            topSuit.text = "?"
+            cardValue.text = "?"
+            bottomSuit.text = "?"
+        }
     }
     
     
