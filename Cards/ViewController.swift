c91e09cea4ab61f96471a5c0921c54f14e9d8da9
ittakesii <julian@alumni.american.edu>
2017-02-11 19:17:38 -0500

    Working

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var clover4: UIButton!
+    @IBOutlet weak var spade3: UIButton!
+    @IBOutlet weak var diamond8: UIButton!
+    @IBOutlet weak var heart10: UIButton!
+    
+    
+    @IBOutlet weak var centerLabel: UILabel!
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +30,31 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
-
-  
-
+    
+    @IBAction func updateLabels(_ sender: UIButton) {
+    
+        
+        switch sender {
+        case clover4:
+            centerLabel.text = "4"
+            topLeft.text = "♣️"
+            bottomRight.text = "♣️"
+        case spade3:
+            centerLabel.text = "3"
+            topLeft.text = "♠️"
+            bottomRight.text = "♠️"
+        case diamond8:
+            centerLabel.text = "8"
+            topLeft.text = "♦️"
+            bottomRight.text = "♦️"
+        case heart10:
+            centerLabel.text = "10"
+            topLeft.text = "♥️"
+            bottomRight.text = "♥️"
+        default:
+        centerLabel.text = "?"
+        topLeft.text = "?"
+        bottomRight.text = "?"
+    }
+    }
 }
