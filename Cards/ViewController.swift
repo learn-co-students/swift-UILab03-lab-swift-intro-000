eeb76e1024a24e9d4de0dcf8ba09733b955aa742
JoelH05 <j_h_olla@hotmail.com>
2017-02-17 19:41:09 -0500

    Completed UILab 03

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,15 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var middle: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
+    @IBOutlet weak var fourClubs: UIButton!
+    @IBOutlet weak var threeSpades: UIButton!
+    @IBOutlet weak var eightDiamonds: UIButton!
+    @IBOutlet weak var tenHearts: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     
+    @IBAction func displayCards(_ sender: UIButton) {
+        
+        switch sender {
+            
+        case fourClubs:
+            topLeft.text = "♣️"
+            middle.text = "4"
+            bottomRight.text = "♣️"
+            
+        case threeSpades:
+            topLeft.text = "♠️"
+            middle.text = "3"
+            bottomRight.text = "♠️"
+            
+        case eightDiamonds:
+            topLeft.text = "♦️"
+            middle.text = "8"
+            bottomRight.text = "♦️"
+            
+        case tenHearts:
+            topLeft.text = "♥️"
+            middle.text = "10"
+            bottomRight.text = "♥️"
+            
+        default:
+            return
+        }
+    }
     
-    // TODO: IB actions and code to update UI
+        
+    
+    
+    
+   
 
   
 
