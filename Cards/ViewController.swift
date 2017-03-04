7bcbfb94da8efb8a5f08a1fb2e7b8e32dd418022
Ryan Lee <rlee33@gmail.com>
2017-03-04 14:36:01 -0600

    final

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,14 +12,44 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var leftCornerSuitLabel: UILabel!
+    @IBOutlet weak var rightCornerSuitLabel: UILabel!
+    @IBOutlet weak var rankLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    
-    
+
     // TODO: IB actions and code to update UI
-
-  
-
+    @IBAction func cardPicker(_ sender: UIButton) {
+        
+        if let buttonTitle = sender.currentTitle {
+            
+            let suit = buttonTitle[buttonTitle.startIndex]
+            let rank = buttonTitle[buttonTitle.index(after: buttonTitle.startIndex)]
+            
+            switch suit {
+            case "♣️":
+                leftCornerSuitLabel.text = "♣️"
+                rightCornerSuitLabel.text = "♣️"
+                rankLabel.text = String(rank)
+            case "♦️":
+                leftCornerSuitLabel.text = "♦️"
+                rightCornerSuitLabel.text = "♦️"
+                rankLabel.text = String(rank)
+            case "♠️":
+                leftCornerSuitLabel.text = "♠️"
+                rightCornerSuitLabel.text = "♠️"
+                rankLabel.text = String(rank)
+            case "♥️":
+                leftCornerSuitLabel.text = "♥️"
+                rightCornerSuitLabel.text = "♥️"
+                rankLabel.text = String(rank)
+            default:
+                print("something went wrong")
+            }
+            
+        }
+    }
 }
