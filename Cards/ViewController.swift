4cf1e5625d0e34e7d3b28ac0768d46779905586c
willsputra <suryadiputrawilliam@gmail.com>
2017-03-04 15:12:59 +0800

    Works

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,12 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
+    @IBOutlet weak var centerLabel: UILabel!
     // TODO: Create IB outlets
     
     override func viewDidLoad() {
+        leftLabel.text = "?"
+        rightLabel.text = "?"
+        centerLabel.text = "?"
         super.viewDidLoad()
     }
     
+    
+    @IBAction func buttonClub(_ sender: Any) {
+        leftLabel.text = "♣️"
+        rightLabel.text = "♣️"
+        centerLabel.text = "4"
+    }
+    
+    @IBAction func buttonSpade(_ sender: Any) {
+        leftLabel.text = "♠️"
+        rightLabel.text = "♠️"
+        centerLabel.text = "3"
+    }
+    
+    @IBAction func buttonDiamond(_ sender: Any) {
+        leftLabel.text = "♦️"
+        rightLabel.text = "♦️"
+        centerLabel.text = "8"
+    }
+
+    @IBAction func buttonHeart(_ sender: Any) {
+        leftLabel.text = "♥️"
+        rightLabel.text = "♥️"
+        centerLabel.text = "10"
+    }
     
     
     // TODO: IB actions and code to update UI
