472c55528e4ebbb49cccb0f664c5c8690a69233a
Alif <saroar9@gmail.com>
2017-03-07 13:51:59 +0300

    solve it

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -15,11 +15,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    
-    
+
     // TODO: IB actions and code to update UI
 
-  
+    @IBOutlet weak var topCorner: UILabel!
+    @IBOutlet weak var middle: UILabel!
+    @IBOutlet weak var downCorner: UILabel!
 
+    func update(middles: String, corners: String) {
+        topCorner.text = corners
+        middle.text = middles
+        downCorner.text = corners
+    }
+
+    @IBAction func fourOfClubes(_ sender: Any) {
+        update(middles: "4", corners: "♣️")
+    }
+
+    @IBAction func threeOfSpades(_ sender: Any) {
+        update(middles: "3", corners: "♠️")
+    }
+
+    @IBAction func eightOfDimonds(_ sender: Any) {
+        update(middles: "8", corners: "♦️")
+    }
+
+    @IBAction func tenOfheartsBtn(_ sender: Any) {
+        update(middles: "10", corners: "♥️")
+    }
 }
