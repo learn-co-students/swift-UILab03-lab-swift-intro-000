579aa0d76cb2328dcfc378610ac8c46ffa031873
Maxwell Benton <maxwellbenton@gmail.com>
2017-03-21 18:29:18 -0400

    ui card lab complete

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,6 +12,35 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var upperLabel: UILabel!
+    @IBOutlet weak var midLabel: UILabel!
+    @IBOutlet weak var lowerLabel: UILabel!
+
+    @IBAction func clubsButton(_ sender: UIButton) {
+        upperLabel.text = "♣️"
+        lowerLabel.text = "♣️"
+        midLabel.text = "4"
+    }
+    
+    @IBAction func spadesButton(_ sender: UIButton) {
+        upperLabel.text = "♠️"
+        lowerLabel.text = "♠️"
+        midLabel.text = "3"
+    }
+    
+    @IBAction func diamondsButton(_ sender: UIButton) {
+        upperLabel.text = "♦️"
+        lowerLabel.text = "♦️"
+        midLabel.text = "8"
+    }
+    
+    @IBAction func heartsButton(_ sender: UIButton) {
+        upperLabel.text = "♥️"
+        lowerLabel.text = "♥️"
+        midLabel.text = "10"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
