541359e2349ba557f5ae900afed73a597f33cd92
Morgan McCauley <morganmccauley@Morgans-MacBook-Pro.local>
2017-04-06 21:55:38 +1200

    uilab 3

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+
+    @IBOutlet weak var leftNumberLabel: UILabel!
+    @IBOutlet weak var rightNumberLabel: UILabel!
+    @IBOutlet weak var centreNumberLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,27 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
+    @IBAction func clubAction(_ sender: Any) {
+        leftNumberLabel.text = "♣️";
+        centreNumberLabel.text = "4";
+        rightNumberLabel.text = "♣️";
+    }
   
+    @IBAction func spadeAction(_ sender: Any) {
+        leftNumberLabel.text = "♠️";
+        centreNumberLabel.text = "3";
+        rightNumberLabel.text = "♠️";
+    }
 
+    @IBAction func diamondAction(_ sender: Any) {
+        leftNumberLabel.text = "♦️";
+        centreNumberLabel.text = "8";
+        rightNumberLabel.text = "♦️";
+    }
+    
+    @IBAction func heartAction(_ sender: Any) {
+        leftNumberLabel.text = "♥️";
+        centreNumberLabel.text = "10";
+        rightNumberLabel.text = "♥️";
+    }
 }
