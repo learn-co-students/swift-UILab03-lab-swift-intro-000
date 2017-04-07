ef0357aff4880a84db2ea933d66272eda4b98a1d
kirillsh93 <kirill.shapoval@gmail.com>
2017-04-07 19:39:31 +0300

    UILab3

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var middleLabel: UILabel!
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        topLabel.text = "?"
+        
+        middleLabel.text = "?"
+        
+        bottomLabel.text = "?"
     }
     
     
     
-    // TODO: IB actions and code to update UI
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let title = sender.titleLabel!.text!
+        
+        switch title {
+        case "♣️4":
+            topLabel.text = "♣️"
+            middleLabel.text = "4"
+            bottomLabel.text = "♣️"
+        case "♠️3":
+            topLabel.text = "♠️"
+            middleLabel.text = "3"
+            bottomLabel.text = "♠️"
+        case "♦️8":
+            topLabel.text = "♦️"
+            middleLabel.text = "8"
+            bottomLabel.text = "♦️"
+        case "♥️10":
+            topLabel.text = "♥️"
+            middleLabel.text = "10"
+            bottomLabel.text = "♥️"
+        default:
+            topLabel.text = "?"
+            middleLabel.text = "?"
+            bottomLabel.text = "?"
+        }
+        
+          }
   
 
 }
