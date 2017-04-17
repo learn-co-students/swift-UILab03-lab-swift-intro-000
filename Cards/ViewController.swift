dc172ef3880873c4ef17eca28cce70900d122a3e
Maneesh <maneesh.yadav@ahold.com>
2017-04-17 22:25:30 +0200

    UI Lab: Cards

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var topLabel: UILabel?
+    @IBOutlet weak var middleLabel: UILabel?
+    @IBOutlet weak var bottomLabel: UILabel?
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,30 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
+//    ♣️4, ♠️3, ♦️8, ♥️10
+    
+    @IBAction func firstButton(sender: UIButton) {
+        topLabel?.text = "♣️"
+        bottomLabel?.text = "♣️"
+        middleLabel?.text = "4"
+    }
+    
+    @IBAction func secondButton(sender: UIButton) {
+        topLabel?.text = "♠️"
+        bottomLabel?.text = "♠️"
+        middleLabel?.text = "3"
+    }
+    
+    @IBAction func thirdButton(sender: UIButton) {
+        topLabel?.text = "♦️"
+        bottomLabel?.text = "♦️"
+        middleLabel?.text = "8"
+    }
   
+    @IBAction func fourthButton(sender: UIButton) {
+        topLabel?.text = "♥️"
+        bottomLabel?.text = "♥️"
+        middleLabel?.text = "10"
+    }
 
 }
