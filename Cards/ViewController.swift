bdec548693be1e45228f1a97888f37a374317b65
Allan Wolff <agwolff@gmail.com>
2017-04-17 14:56:13 -0300

    resolved exercise cards

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,14 +12,63 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var centralLabel: UILabel!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button10: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    // functions
     
+    func updateCard(button: UIButton) {
+        switch button {
+        case button4:
+            topLabel.text = "♣️"
+            bottomLabel.text = "♣️"
+            centralLabel.text = "4"
+        case button3:
+            topLabel.text = "♠️"
+            bottomLabel.text = "♠️"
+            centralLabel.text = "3"
+        case button8:
+            topLabel.text = "♦️"
+            bottomLabel.text = "♦️"
+            centralLabel.text = "8"
+            centralLabel.textColor = UIColor.red
+        case button10:
+            topLabel.text = "♥️"
+            bottomLabel.text = "♥️"
+            centralLabel.text = "10"
+            centralLabel.textColor = UIColor.red
+        default:
+            break
+        }
+    }
     
     
     // TODO: IB actions and code to update UI
 
+    @IBAction func clickButtonCard(_ sender: UIButton) {
+        switch sender {
+        case button4:
+            updateCard(button: sender)
+        case button3:
+            updateCard(button: sender)
+        case button8:
+            updateCard(button: sender)
+        case button10:
+            updateCard(button: sender)
+        default:
+            break
+        }
+        
+    }
   
 
 }
