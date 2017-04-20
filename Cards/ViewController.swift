5f49e410cd882d1a1d7024dcabcafc06764e8fab
brianpoole <game.programmer@gmail.com>
2017-04-20 10:32:33 -0700

    solved

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var topLeftSuit: UILabel!
+    @IBOutlet weak var cardValue: UILabel!
+    @IBOutlet weak var bottomRightSuit: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,15 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
+    @IBAction func changeCard(_ sender: UIButton) {
+        let currentTitle: String = sender.currentTitle!
+        let str_split = currentTitle.index(currentTitle.startIndex, offsetBy: 1)
+        let currentSuit = sender.currentTitle?.substring(to: str_split)
+        let currentCard = sender.currentTitle?.substring(from: str_split)
+        topLeftSuit.text = currentSuit
+        bottomRightSuit.text = currentSuit
+        cardValue.text = currentCard
+    }
 
   
 
