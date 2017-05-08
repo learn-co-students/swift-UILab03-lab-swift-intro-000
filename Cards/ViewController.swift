4f835b0e4b83478510b625e5944c75eaf5ddda7f
Panagiotis <pskarl@gmail.com>
2017-05-08 21:19:57 +0300

    Done.

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var labelTop: UILabel!
+    @IBOutlet weak var labelBottom: UILabel!
+    @IBOutlet weak var labelMain: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +24,27 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
+ 
+    @IBAction func four(_ sender: Any) {
+        labelTop.text = "♣️"
+        labelBottom.text = "♣️"
+        labelMain.text = "4"
+    }
+    @IBAction func three(_ sender: Any) {
+        labelTop.text = "♠️"
+        labelBottom.text = "♠️"
+        labelMain.text = "3"
+    }
+    @IBAction func eight(_ sender: Any) {
+        labelTop.text = "♦️"
+        labelBottom.text = "♦️"
+        labelMain.text = "8"
+    }
+    @IBAction func ten(_ sender: Any) {
+        labelTop.text = "♥️"
+        labelBottom.text = "♥️"
+        labelMain.text = "10"
+    }
 
   
 
