6af91bd06ecb6dc4353d6c6a8a714e5e391639f9
one-for-all <jianan.li@yahoo.com>
2017-05-12 15:19:57 -0700

    Implemented Cards selection problems

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var upperLeftLabel: UILabel!
+    @IBOutlet weak var centerLabel: UILabel!
+    @IBOutlet weak var lowerRightLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,12 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
-
-  
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let buttonText = sender.currentTitle!
+        upperLeftLabel.text = "\(buttonText[buttonText.startIndex])"
+        lowerRightLabel.text = "\(buttonText[buttonText.startIndex])"
+        let numberRange = buttonText.index(buttonText.startIndex, offsetBy: 1)..<buttonText.endIndex
+        centerLabel.text = buttonText[numberRange]
+    }
 
 }
