3a86ac315bca7c348cca97e6f71e3791e33fba52
tamarkandathi <tamar.kandathi@gmail.com>
2017-05-26 15:21:03 -0400

    CardsLab -done

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    @IBOutlet weak var suitLabelUpperLeft: UILabel!
+    @IBOutlet weak var suitLabelLowerRight: UILabel!
+    @IBOutlet weak var rankLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        suitLabelUpperLeft.text = "?"
+        suitLabelLowerRight.text = "?"
+        rankLabel.text = "?"
+    }
+    
+    func updateSuitLabels(suit: String) -> Void {
+        suitLabelUpperLeft.text = suit
+        suitLabelLowerRight.text = suit
     }
     
     
-    
-    // TODO: IB actions and code to update UI
-
-  
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        switch sender.currentTitle! {
+        case "♣︎4":
+            rankLabel.text = "4"
+            updateSuitLabels(suit: "♣︎")
+        case "♠︎4":
+            rankLabel.text = "4"
+            updateSuitLabels(suit: "♠︎")
+        case "♦︎8":
+            rankLabel.text = "8"
+            updateSuitLabels(suit: "♦︎")
+        case "♥︎10":
+            rankLabel.text = "10"
+            updateSuitLabels(suit: "♥︎")
+        default:
+            rankLabel.text = "?"
+            updateSuitLabels(suit: "?")
+        }
+    }
 }
