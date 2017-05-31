5eba9619ef88c3e334d59d0d2baab634aaf6aa42
bebopjazz07 <branden.johnson@gmail.com>
2017-05-31 15:03:49 -0500

    Card Lab complete

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,14 +12,41 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var centerLabel: UILabel!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    @IBOutlet weak var fourClubs: UIButton!
+    @IBOutlet weak var threeSpades: UIButton!
+    @IBOutlet weak var eightDiamonds: UIButton!
+    @IBOutlet weak var tenHearts: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
-    
-    // TODO: IB actions and code to update UI
-
-  
-
+    @IBAction func cardSwitch(_ sender: UIButton) {
+        switch sender {
+        case fourClubs:
+            topLabel.text = "♣️"
+            bottomLabel.text = "♣️"
+            centerLabel.text = "4"
+        case threeSpades:
+            topLabel.text = "♠️"
+            bottomLabel.text = "♠️"
+            centerLabel.text = "3"
+        case eightDiamonds:
+            topLabel.text = "♦️"
+            bottomLabel.text = "♦️"
+            centerLabel.text = "8"
+        case tenHearts:
+            topLabel.text = "♥️"
+            bottomLabel.text = "♥️"
+            centerLabel.text = "10"
+        default:
+            topLabel.text = ""
+            bottomLabel.text = ""
+            centerLabel.text = ""
+        }
+    }
 }
