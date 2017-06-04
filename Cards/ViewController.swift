ede87f85c5ff2eabdda01501889cda992397c894
Garett Arrowood <garettarrowood@gmail.com>
2017-06-04 14:41:10 -0400

    Can not share a IBOutlet between 2 UILabels

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -1,25 +1,35 @@
-//
-//  ViewController.swift
-//  Cards
-//
-//  Created by Jim Campagno on 9/17/16.
-//  Copyright © 2016 Flatiron School. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
-    
+    @IBOutlet weak var suitLabel: UILabel!
+    @IBOutlet weak var otherSuitLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    
-    
-    // TODO: IB actions and code to update UI
 
-  
+    @IBAction func club4(_ sender: Any) {
+        updateLabels(suit: "♣️", value: "4")
+    }
 
+    @IBAction func spade3(_ sender: Any) {
+        updateLabels(suit: "♠️", value: "3")
+    }
+
+    @IBAction func diamond8(_ sender: Any) {
+        updateLabels(suit: "♦️", value: "8")
+    }
+
+    @IBAction func heart10(_ sender: Any) {
+        updateLabels(suit: "♥️", value: "10")
+    }
+
+    func updateLabels(suit: String, value: String) {
+        suitLabel.text = suit
+        otherSuitLabel.text = suit
+        numberLabel.text = value
+    }
 }
