e6d1488dd886d6545e17f811907e1ace0fea99fa
iBrie <ibrigitte@me.com>
2017-06-12 15:42:52 +0200

    kaartspel

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,15 +11,42 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
-    
+  @IBOutlet weak var suitLinksBoven: UILabel!
+  @IBOutlet weak var suitRechtsOnder: UILabel!
+  @IBOutlet weak var valueMidden: UILabel!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     
+  @IBAction func knopEen(_ sender: Any) {
+    
+    suitLinksBoven.text = " ♣️ "
+    suitRechtsOnder.text = " ♣️ "
+    valueMidden.text = "4"
+  }
+  @IBAction func knopTwee(_ sender: Any) {
+    
+    suitLinksBoven.text = " ♠️ "
+    suitRechtsOnder.text = " ♠️ "
+    valueMidden.text = "3"
+  }
+  @IBAction func knopDrie(_ sender: Any) {
+    
+    suitLinksBoven.text = " ♦️ "
+    suitRechtsOnder.text = " ♦️ "
+    valueMidden.text = "8"
+  }
+  @IBAction func knopVier(_ sender: Any) {
+    
+    suitLinksBoven.text = " ♥️ "
+    suitRechtsOnder.text = " ♥️ "
+    valueMidden.text = "10"
+  }
     
     // TODO: IB actions and code to update UI
 
-  
+ 
 
 }
