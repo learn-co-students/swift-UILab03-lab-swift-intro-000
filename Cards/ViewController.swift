28072ee03bc43103d059eddc781a49c809638a38
Jing Lin Ruan <jinglinruan@gmail.com>
2017-06-15 16:59:29 -0400

    Cards

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -16,10 +16,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+
+    @IBOutlet weak var value: UILabel!
+    @IBOutlet weak var upperSuit: UILabel!
+    @IBOutlet weak var lowerSuit: UILabel!
     
     
     // TODO: IB actions and code to update UI
 
-  
+    @IBAction func clubButton(_ sender: UIButton) {
+        value.text = "4"
+        lowerSuit.text = "♣️"
+        upperSuit.text = "♣️"
+    }
+    
+    @IBAction func spadeButton(_ sender: UIButton) {
+        value.text = "3"
+        lowerSuit.text = "♠️"
+        upperSuit.text = "♠️"
+    }
+    @IBAction func diamondButton(_ sender: UIButton) {
+        value.text = "8"
+        lowerSuit.text = "♦️"
+        upperSuit.text = "♦️"
+    }
+    @IBAction func heartButton(_ sender: UIButton) {
+        value.text = "10"
+        lowerSuit.text = "♥️"
+        upperSuit.text = "♥️"
+    }
+    
+    
 
 }
