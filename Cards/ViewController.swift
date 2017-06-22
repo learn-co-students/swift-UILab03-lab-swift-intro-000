f467f87d37cd4b7e2c6240dd63ffaf1f13ab52e6
terminalzzz <zliu.msb@gmail.com>
2017-06-21 23:21:34 -0400

    my commit

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,10 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var suitesBottom: UILabel!
+    @IBOutlet weak var suitesTop: UILabel!
+    
+    @IBOutlet weak var club4: UIButton!
+    @IBOutlet weak var spade3: UIButton!
+    @IBOutlet weak var diamond8: UIButton!
+    @IBOutlet weak var heart10: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func btnPressed(_ sender: UIButton) {
+        print(sender.restorationIdentifier)
+        
+        let btnSelected:String = sender.restorationIdentifier!
+        
+        if btnSelected.contains("1") {
+            suitesTop.text = "♣️"
+            suitesBottom.text = "♣️"
+            number.text = "4"
+        }
+        if btnSelected.contains("2") {
+            suitesTop.text = "♠️"
+            suitesBottom.text = "♠️"
+            number.text = "3"
+        }
+        if btnSelected.contains("3") {
+            suitesTop.text = "♦️"
+            suitesBottom.text = "♦️"
+            number.text = "8"
+        }
+        if btnSelected.contains("4") {
+            suitesTop.text = "♥️"
+            suitesBottom.text = "♥️"
+            number.text = "10"
+        }
+        
+        
     }
     
     
