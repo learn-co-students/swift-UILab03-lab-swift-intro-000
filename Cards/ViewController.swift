fcc7c3109b92d57dd7c19a577aa5cc2da24e3e09
Hansel Mationg <hansel.mationg@gmail.com>
2017-07-24 14:50:17 +0800

    finished lab

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -16,10 +16,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    @IBOutlet weak var suit: UILabel!
+    @IBOutlet weak var suit2: UILabel!
+    @IBOutlet weak var value: UILabel!
     
     
     // TODO: IB actions and code to update UI
+    func updateDisplay (suits: String, values: String) {
+        suit.text = suits
+        suit2.text = suits
+        value.text = values
+    }
+    
+    @IBAction func clubFour(_ sender: UIButton) {
+        updateDisplay(suits: "♣️", values: "4")
+    }
+    
+    @IBAction func spadeThree(_ sender: UIButton) {
+        updateDisplay(suits: "♠️", values: "3")
+    }
 
+    @IBAction func diamondEight(_ sender: UIButton) {
+        updateDisplay(suits: "♦️", values: "8")
+    }
+    
+    @IBAction func heartTen(_ sender: UIButton) {
+        updateDisplay(suits: "❤️", values: "10")
+    }
+    
   
 
 }
