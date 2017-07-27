56c603bd51c4a90225b3e3f5ac6a369253f2ed21
dgmalot <dgmalot@gmail.com>
2017-07-27 13:54:23 -0400

    Ooh yeah I like the buttons.

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    
+    // labels
+    @IBOutlet weak var suitUpperLeft: UILabel!
+    @IBOutlet weak var suitLowerRight: UILabel!
+    @IBOutlet weak var facevalueCenter: UILabel!
+    
+    // buttons
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var fourthButton: UIButton!
+    
+    // other
+    let defaultLabel = "?"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        suitUpperLeft.text = defaultLabel
+        suitLowerRight.text = defaultLabel
+        facevalueCenter.text = defaultLabel
     }
-    
-    
-    
-    // TODO: IB actions and code to update UI
-
   
+    @IBAction func suitSelection(_ sender: UIButton) {
+        switch sender {
+        case firstButton:
+            suitUpperLeft.text = "♣️"
+            suitLowerRight.text = "♣️"
+            facevalueCenter.text = "4"
+        case secondButton:
+            suitUpperLeft.text = "♠️"
+            suitLowerRight.text = "♠️"
+            facevalueCenter.text = "3"
+        case thirdButton:
+            suitUpperLeft.text = "♦️"
+            suitLowerRight.text = "♦️"
+            facevalueCenter.text = "8"
+        case fourthButton:
+            suitUpperLeft.text = "♥️"
+            suitLowerRight.text = "♥️"
+            facevalueCenter.text = "10"
+        default:
+            print("Oops something went wrong...")
+        }
+    }
 
 }
