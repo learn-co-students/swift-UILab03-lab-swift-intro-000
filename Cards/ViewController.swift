54e149b48e4defd52f615ccc02ce0ae4a63d7724
dryng <dryngler@gmail.com>
2017-08-07 00:08:21 -0700

    Done

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var clover4: UIButton!
+    @IBOutlet weak var spade3: UIButton!
+    @IBOutlet weak var diamond8: UIButton!
+    @IBOutlet weak var heart10: UIButton!
+    @IBOutlet weak var topLeftLabel: UILabel!
+    @IBOutlet weak var buttomRightLabel: UILabel!
+    @IBOutlet weak var middleLabel: UILabel!
 
     // TODO: Create IB outlets
     
@@ -16,7 +24,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+        func updateDisplay (suits: String, values: String) {
+            topLeftLabel.text = suits
+            buttomRightLabel.text = suits
+            middleLabel.text = values
+            
+            }
+        
+    @IBAction func clover4(_ sender: UIButton) {
+        updateDisplay(suits: "♣️", values: "4")
+
+    }
+        
+    @IBAction func spade3(_ sender: UIButton) {
+        updateDisplay(suits: "♠️", values: "3")
+    }
     
+    @IBAction func diamond8(_ sender: UIButton) {
+        updateDisplay(suits: "♦️", values: "8")
+    }
+        
+    @IBAction func heart10(_ sender: UIButton) {
+        updateDisplay(suits: "❤️", values: "10")
+    }
     
     // TODO: IB actions and code to update UI
 
