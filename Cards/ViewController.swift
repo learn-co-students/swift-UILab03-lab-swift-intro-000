4cf28bb6f38ed3dd3fa0d530887d5ca3e0142cbb
Shmira <11msm7@queensu.ca>
2017-08-18 12:51:40 -0700

    Cards work

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var topLeft: UILabel!
+    
+    @IBOutlet weak var bottomRight: UILabel!
+    
+    @IBOutlet weak var Center: UILabel!
+    
+    @IBOutlet weak var club: UIButton!
+    
+    @IBOutlet weak var spade: UIButton!
+    
+    @IBOutlet weak var diamond: UIButton!
+    
+    @IBOutlet weak var heart: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +33,27 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
-
+    
+    @IBAction func bClub(_ sender: Any) {
+        topLeft.text = "♣️"
+        bottomRight.text = "♣️"
+        Center.text = "4"
+    }
+    @IBAction func bSpade(_ sender: Any) {
+        topLeft.text = "♠️"
+        bottomRight.text = "♠️"
+        Center.text = "3"
+    }
+    @IBAction func bDiamond(_ sender: Any) {
+        topLeft.text = "♦️"
+        bottomRight.text = "♦️"
+        Center.text = "8"
+    }
+    @IBAction func bHeart(_ sender: Any) {
+        topLeft.text = "♥️"
+        bottomRight.text = "♥️"
+        Center.text = "10"
+    }
   
 
 }
