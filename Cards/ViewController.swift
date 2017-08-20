8bdb282663f4d3278af8458305230ccdc0a7f354
regina02 <rlarcilla@hotmail.com>
2017-08-19 20:41:39 -0700

    finished cards lab

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    
+    @IBOutlet weak var club: UIButton!
+    
+    @IBOutlet weak var spade: UIButton!
+    
+    @IBOutlet weak var diamond: UIButton!
+    
+    @IBOutlet weak var heart: UIButton!
+    
+    @IBOutlet weak var leftLabel: UILabel!
+    
+    @IBOutlet weak var rightLabel: UILabel!
+    
+    @IBOutlet weak var bigLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        bigLabel.isHidden = true
+        leftLabel.isHidden = true
+        rightLabel.isHidden = true
     }
     
     
     
-    // TODO: IB actions and code to update UI
-
-  
+    
+    @IBAction func buttonPressed(_ sender: UIButton!) {
+        
+        switch(sender){
+            case club:
+                bigLabel.text = "4"
+                bigLabel.isHidden = false
+                rightLabel.text = "♣️"
+                rightLabel.isHidden = false
+                leftLabel.text = "♣️"
+                leftLabel.isHidden = false
+                break;
+            case spade:
+                bigLabel.text = "3"
+                bigLabel.isHidden = false
+                rightLabel.text = "♠️"
+                rightLabel.isHidden = false
+                leftLabel.text = "♠️"
+                leftLabel.isHidden = false
+                break;
+            case diamond:
+                bigLabel.text = "8"
+                bigLabel.isHidden = false
+                rightLabel.text = "♦️"
+                rightLabel.isHidden = false
+                leftLabel.text = "♦️"
+                leftLabel.isHidden = false
+                break;
+            case heart:
+                bigLabel.text = "10"
+                bigLabel.isHidden = false
+                rightLabel.text = "♥️"
+                rightLabel.isHidden = false
+                leftLabel.text = "♥️"
+                leftLabel.isHidden = false
+                break;
+            default:
+                break;
+        }
+        
+        
+    }
 
 }
