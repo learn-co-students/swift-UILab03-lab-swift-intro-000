26d87a9d075bde843b1f8b4ebb3be3463f4d4aff
srigirishankar <srigirishankar@gmail.com>
2017-08-22 23:26:21 -0500

    Cards done

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,6 +12,39 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var centerLabel: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
+    
+    
+    @IBAction func startCardsGame(_ sender: UIButton) {
+        let arrayOfOptions = [4, 3, 8, 10]
+        let randomChoice = Int(arc4random_uniform(UInt32(arrayOfOptions.count)))
+        switch randomChoice {
+        case 0:
+            topLeft.text = "♣️"
+            centerLabel.text = "4"
+            bottomRight.text = "♣️"
+        case 1:
+            topLeft.text = "♠️"
+            centerLabel.text = "3"
+            bottomRight.text = "♠️"
+        case 2:
+            topLeft.text = "♦️"
+            centerLabel.text = "8"
+            bottomRight.text = "♦️"
+        case 3:
+            topLeft.text = "♥️"
+            centerLabel.text = "10"
+            bottomRight.text = "♥️"
+        default:
+            topLeft.text = "?"
+            centerLabel.text = "?"
+            bottomRight.text = "?"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
