9f43276b0df2380d02f7ea2cfae4a81e0916809c
Yue Feng <strangedigital@gmail.com>
2017-09-29 20:48:26 -0400

    Done

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,13 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    @IBOutlet weak var topS:UILabel!
+    @IBOutlet weak var botS:UILabel!
+    @IBOutlet weak var midS:UILabel!
     
     override func viewDidLoad() {
+        topS.text = " "
+        botS.text = " "
+        midS.text = " "
         super.viewDidLoad()
+    }
+
+    func changeup(_ si:String, _ nu:Int) {
+        topS.text = si
+        botS.text = si
+        midS.text = String(nu)
     }
     
     
+    @IBAction func firstButPress(_ sender: UIButton) {
+        changeup("♣️",4)
+    }
+    
+    @IBAction func secondButPress(_ sender: UIButton) {
+        changeup("♠️",3)
+    }
+    
+    
+    @IBAction func thirdButPress(_ sender: UIButton) {
+        changeup("♦️",8)
+    }
+    
+    
+    @IBAction func fourthButPress(_ sender: Any) {
+        changeup("♥️",10)
+    }
     
     // TODO: IB actions and code to update UI
 
