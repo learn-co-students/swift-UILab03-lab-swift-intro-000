36f5c3d3007720087131461c0e4adc5f9880e034
keith1015 <chungagk@gmail.com>
2017-10-08 18:23:44 -0400

    Lab

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var topCorner: UILabel!
+    @IBOutlet weak var bottomCorner: UILabel!
+    @IBOutlet weak var center: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,37 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
-
+    @IBAction func button(_ sender: UIButton)
+    {
+        topCorner.text = "♣️"
+        bottomCorner.text = "♣️"
+        center.text = "4"
+    }
+    
+    @IBAction func button2(_ sender: UIButton)
+    {
+        topCorner.text = "♠️"
+        bottomCorner.text = "♠️"
+        center.text = "3"
+    }
+    
+    @IBAction func button3(_ sender: UIButton)
+    {
+        topCorner.text = "♦️"
+        bottomCorner.text = "♦️"
+        center.text = "8"
+    }
+    
+    @IBAction func button4(_ sender: UIButton)
+    {
+        topCorner.text = "♥️"
+        bottomCorner.text = "♥️"
+        center.text = "10"
+    }
+    
+    
+    
+    
   
 
 }
