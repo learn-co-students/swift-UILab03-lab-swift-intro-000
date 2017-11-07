1ef8f27818f873bbb67cf0e8081c12d38240a760
asyzdykov <internetcafe@bk.ru>
2017-11-08 01:54:09 +0600

    worksaswell

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,7 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftCornerLabel: UILabel!
+    @IBOutlet weak var rightCornerLabel: UILabel!
+    @IBOutlet weak var middleLabel: UILabel!
     // TODO: Create IB outlets
+    @IBOutlet weak var clubs4: UIButton!
+    @IBOutlet weak var spades3: UIButton!
+    @IBOutlet weak var diamonds8: UIButton!
+    @IBOutlet weak var hearts10: UIButton!
+    
+    @IBAction func cardSelected(_ sender: UIButton) {
+        switch sender {
+        case clubs4:
+            leftCornerLabel.text = "4"
+            rightCornerLabel.text = "4"
+            middleLabel.text = "♣️"
+        case spades3:
+            leftCornerLabel.text = "3"
+            rightCornerLabel.text = "3"
+            middleLabel.text = "♠️"
+        case diamonds8:
+            leftCornerLabel.text = "8"
+            rightCornerLabel.text = "8"
+            middleLabel.text = "♦️"
+         default:
+            leftCornerLabel.text = "10"
+            rightCornerLabel.text = "10"
+            middleLabel.text = "♥️"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
