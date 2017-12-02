ef42a386f2a3d277a859edbc9c008dd19ed8ce94
Alex <onischenko_ay@inbox.ru>
2017-12-02 15:26:32 +0300

    done

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,10 +12,46 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var UpperType: UILabel!
+    @IBOutlet weak var LowerType: UILabel!
+    @IBOutlet weak var centerText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func swordsFour(_ sender: Any) {
+        UpperType.text = "♣️"
+        LowerType.text = "♣️"
+        UpperType.textColor = UIColor.black
+        LowerType.textColor = UIColor.black
+        centerText.text = "4"
+    }
+    
+    
+    @IBAction func pikesThree(_ sender: Any) {
+        UpperType.text = "♠️"
+        LowerType.text = "♠️"
+        UpperType.textColor = UIColor.black
+        LowerType.textColor = UIColor.black
+        centerText.text = "3"
+    }
+    
+    @IBAction func diamondsEight(_ sender: Any) {
+        UpperType.text = "♦️"
+        LowerType.text = "♦️"
+        UpperType.textColor = UIColor.red
+        LowerType.textColor = UIColor.red
+        centerText.text = "8"
+    }
+    
+    @IBAction func heartsTen(_ sender: Any) {
+        UpperType.text = "♥️"
+        LowerType.text = "♥️"
+        UpperType.textColor = UIColor.red
+        LowerType.textColor = UIColor.red
+        centerText.text = "10"
+    }
     
     
     // TODO: IB actions and code to update UI
