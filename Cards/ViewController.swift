c554180d454084fefebf175687af378d4150b810
Tor Magnus Michaelsen <tormagnusmichaelsen@Tors-MacBook-Pro.local>
2017-12-23 21:55:12 +0100

    finito

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,14 +12,36 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var corner1: UILabel!
+    @IBOutlet weak var corner2: UILabel!
+    @IBOutlet weak var midLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
-    
     // TODO: IB actions and code to update UI
-
-  
-
+    @IBAction func button1(_ sender: Any) {
+        corner1.text = "♣️"
+        corner2.text = "♣️"
+        midLabel.text = "4"
+    }
+    @IBAction func button2(_ sender: Any) {
+        corner1.text = "♠️"
+        corner2.text = "♠️"
+        midLabel.text = "3"
+    }
+    @IBAction func button3(_ sender: Any) {
+        corner1.text = "♥️"
+        corner2.text = "♥️"
+        midLabel.text = "8"
+    }
+    @IBAction func button4(_ sender: Any) {
+        corner1.text = "♦️"
+        corner2.text = "♦️"
+        midLabel.text = "10"
+    }
+    
+    
+    
 }
