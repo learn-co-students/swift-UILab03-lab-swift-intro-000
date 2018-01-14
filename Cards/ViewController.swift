9f3bdbae8a42794e8ba2b917c8da2ffaf85857dd
Jaldhar H. Vyas <jaldhar@braincells.com>
2018-01-14 13:42:02 -0500

    Complete.

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,7 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
-    
+    @IBOutlet weak var tlSuit: UILabel!
+    @IBOutlet weak var brSuit: UILabel!
+    @IBOutlet weak var value: UILabel!
+    @IBOutlet weak var clubs: UIButton!
+    @IBOutlet weak var spades: UIButton!
+    @IBOutlet weak var diamonds: UIButton!
+    @IBOutlet weak var hearts: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -20,6 +27,33 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
-  
+    @IBAction func handleButtons(_ sender: Any) {
+
+        switch sender as! UIButton {
+        case clubs:
+            tlSuit.text = "♣️"
+            brSuit.text = "♣️"
+            value.text = "4"
+            break
+        case spades:
+            tlSuit.text = "♠️"
+            brSuit.text = "♠️"
+            value.text = "3"
+            break
+        case diamonds:
+            tlSuit.text = "♦️"
+            brSuit.text = "♦️"
+            value.text = "8"
+            break
+        case hearts:
+            tlSuit.text = "❤️"
+            brSuit.text = "❤️"
+            value.text = "10"
+            break
+        default:
+            break
+        }
+    }
+    
 
 }
