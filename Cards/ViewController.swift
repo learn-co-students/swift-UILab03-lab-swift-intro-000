7f92c169d80abeb3c92f9928d80e43ff0f1fe871
dylanjrafferty <dylanjrafferty@gmail.com>
2018-01-22 12:52:40 -0500

    Complete

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,11 +12,33 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var suits: UILabel!
+    
+    @IBOutlet weak var number: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func heart(_ sender: UIButton) {
+        suits.text = "♥️"
+        number.text = "2"
+    }
     
+    @IBAction func club(_ sender: UIButton) {
+        suits.text = "♣️"
+        number.text = "9"
+    }
+    
+    @IBAction func spade(_ sender: UIButton) {
+        suits.text = "♠️"
+        number.text = "3"
+    }
+    
+    @IBAction func diamond(_ sender: UIButton) {
+        suits.text = "♦️"
+        number.text = "8"
+    }
     
     // TODO: IB actions and code to update UI
 
