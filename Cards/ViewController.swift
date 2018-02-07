c02cb663f9286dcad487affce6ecee58fe8b125b
chenjm1960 <chenjm1960@yahoo.com>
2018-02-06 23:04:57 -0800

    finished

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,15 +11,60 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var midLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    @IBOutlet weak var clubButton: UIButton!
+    @IBOutlet weak var spadeButton: UIButton!
+    @IBOutlet weak var diamondButton: UIButton!
+    @IBOutlet weak var heartButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    var tempArray:[Any] = []
     
-    
-    // TODO: IB actions and code to update UI
-
-  
-
+    @IBAction func clubPress(_ sender: UIButton) {
+        let clubString = sender.titleLabel!.text! as String
+        for i in clubString {
+            tempArray.append(i)
+        }
+        topLabel.text = "\(tempArray[0])"
+        bottomLabel.text = "\(tempArray[0])"
+        midLabel.text = "\(tempArray[1])"
+        tempArray = []
+    }
+    @IBAction func spadePress(_ sender: UIButton) {
+        let spadeString = sender.titleLabel!.text! as String
+        for i in spadeString {
+            tempArray.append(i)
+        }
+        topLabel.text = "\(tempArray[0])"
+        bottomLabel.text = "\(tempArray[0])"
+        midLabel.text = "\(tempArray[1])"
+        tempArray = []
+    }
+    @IBAction func diamondPress(_ sender: UIButton) {
+        let diamondString = sender.titleLabel!.text! as String
+        for i in diamondString {
+            tempArray.append(i)
+        }
+        topLabel.text = "\(tempArray[0])"
+        bottomLabel.text = "\(tempArray[0])"
+        midLabel.text = "\(tempArray[1])"
+        tempArray = []
+    }
+    @IBAction func heartPress(_ sender: UIButton) {
+        let heartString = sender.titleLabel!.text! as String
+        for i in heartString {
+            tempArray.append(i)
+        }
+        topLabel.text = "\(tempArray[0])"
+        bottomLabel.text = "\(tempArray[0])"
+        midLabel.text = "\(tempArray[1])"
+        tempArray = []
+    }
 }
