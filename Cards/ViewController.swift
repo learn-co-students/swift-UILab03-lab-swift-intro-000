aa527d56864529f745a1e9025099174e922c739b
Kun Huang <kkun.huang@gmail.com>
2018-03-23 13:50:40 -0400

    Cards

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var middle: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
+    
     // TODO: Create IB outlets
     
     override func viewDidLoad() {
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
-  
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        var num = sender.currentTitle!
+        let symbol = num.remove(at: num.startIndex)
+
+        topLeft.text = String(symbol)
+        middle.text = String(num)
+        bottomRight.text = String(symbol)
+        
+        
+    }
+    
 
 }
