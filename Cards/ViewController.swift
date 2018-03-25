55c1c59da3af47bb965c4bd5e0b52165c7ff85d5
sheziasn <sheziasn1@hotmail.com>
2018-03-25 17:26:52 +0300

    done

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var center: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
     
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +24,31 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
-
+    @IBAction func fourFlower(_ sender: Any) {
+        center.text = "4"
+        topLeft.text = "♣️"
+        bottomRight.text = "♣️"
+    }
+    
+    @IBAction func threePaan(_ sender: Any) {
+        center.text = "3"
+        topLeft.text = "♠️"
+        bottomRight.text = "♠️"
+    }
+    
+    @IBAction func eightBrick(_ sender: Any) {
+        center.text = "8"
+        topLeft.text = "♦️"
+        bottomRight.text = "♦️"
+    }
+    
+    @IBAction func tenHeart(_ sender: Any) {
+        center.text = "10"
+        topLeft.text = "♥️"
+        bottomRight.text = "♥️"
+    }
+    
+    
   
 
 }
