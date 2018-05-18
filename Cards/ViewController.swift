984d1a48ba3e2b5d3d4983ace0bdc667d43bece1
winzardv11 <ridzwan.midris@gmail.com>
2018-05-18 15:37:00 +0800

    Simple

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var labelTop: UILabel!
+    @IBOutlet weak var labelBottom: UILabel!
+    @IBOutlet weak var labelMiddle: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,31 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
-  
+    @IBAction func buttonClub(_ sender: Any) {
+        labelTop.text = "♣️"
+        labelBottom.text = "♣️"
+        labelMiddle.text = "4"
+    }
+    
+    @IBAction func buttonSpade(_ sender: Any) {
+        labelTop.text = "♠️"
+        labelBottom.text = "♠️"
+        labelMiddle.text = "3"
+    }
+    @IBAction func buttonDiamond(_ sender: Any) {
+        labelTop.text = "♦️"
+        labelBottom.text = "♦️"
+        labelMiddle.text = "8"
+    }
+    @IBAction func buttonHeart(_ sender: Any) {
+        labelTop.text = "♥️"
+        labelBottom.text = "♥️"
+        labelMiddle.text = "10"
+    }
+    
+        
+        
+    
+    
 
 }
