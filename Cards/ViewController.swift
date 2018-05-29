40c04508a5373ee8247a348049a9fb31cdf5db76
Jayymash <mashnijason@gmail.com>
2018-05-29 11:11:46 -0400

    Easy Peazy Lemon Squeezy

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    @IBOutlet weak var centerLabel: UILabel!
+    @IBOutlet weak var topCornerLabel: UILabel!
+    @IBOutlet weak var bottomCornerLabel: UILabel!
+    
+    func unhideCorners(Emoji: String) {
+        topCornerLabel.isHidden = false
+        topCornerLabel.text = Emoji
+        bottomCornerLabel.isHidden = false
+        bottomCornerLabel.text = Emoji
+    }
+
+    func unhideCenter(Value: String) {
+        centerLabel.isHidden = false
+        centerLabel.text = Value
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        topCornerLabel.isHidden = true
+        centerLabel.isHidden = true
+        bottomCornerLabel.isHidden = true
     }
     
     
-    
-    // TODO: IB actions and code to update UI
-
-  
-
+    @IBAction func cloverButtonTap(_ sender: Any) {
+        unhideCenter(Value: "4")
+        unhideCorners(Emoji:  "♣︎")
+    }
+    @IBAction func spadeButtonTap(_ sender: Any) {
+        unhideCenter(Value: "3")
+        unhideCorners(Emoji: "♠︎")
+    }
+    @IBAction func diamondButtonTap(_ sender: Any) {
+        unhideCenter(Value: "8")
+        unhideCorners(Emoji: "♦︎")
+    }
+    @IBAction func heartButtonTap(_ sender: Any) {
+        unhideCenter(Value: "10")
+        unhideCorners(Emoji: "♥︎")
+    }
 }
