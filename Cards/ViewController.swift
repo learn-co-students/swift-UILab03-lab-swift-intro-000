3096d8f8e2b5d6de554e5125907658ebf1200f67
Gustavo Picciuto <gustavo.picciutomaury@live.com>
2018-09-25 17:01:16 -0400

    Complete problems

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,15 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    @IBOutlet weak var topLeftLabel: UILabel!
+    @IBOutlet weak var rightBottomLabel: UILabel!
+    @IBOutlet weak var centerLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func cardButtons(_ sender: UIButton) {
+        switch sender.tag{
+        case 0:
+            topLeftLabel.text = "♣️"
+            rightBottomLabel.text = "♣️"
+            centerLabel.text = "4"
+        case 1:
+            topLeftLabel.text = "♠️"
+            rightBottomLabel.text = "♠️"
+            centerLabel.text = "3"
+        case 2:
+            topLeftLabel.text = "♦️"
+            rightBottomLabel.text = "♦️"
+            centerLabel.text = "8"
+        case 3:
+            topLeftLabel.text = "♥️"
+            rightBottomLabel.text = "♥️"
+            centerLabel.text = "10"
+        default:
+            return
+            
+        }
+        
+    }
     
-    
-    // TODO: IB actions and code to update UI
 
   
 
