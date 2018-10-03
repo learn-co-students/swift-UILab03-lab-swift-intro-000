4761e8450df20d5fdb13b51710690e1233ccb806
nalamoudi <nahoorii_91@hotmail.com>
2018-10-03 14:27:28 +0300

    done cards

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,14 +12,44 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
+    @IBOutlet weak var centerLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     
+    @IBAction func Clubs(_ sender: Any) {
+        topLeft.text = "4"
+        bottomRight.text = "4"
+        centerLabel.text = "♣️"
+    }
     
-    // TODO: IB actions and code to update UI
-
+    
+    @IBAction func Spades(_ sender: Any) {
+        topLeft.text = "3"
+        bottomRight.text = "3"
+        centerLabel.text = "♠️"
+    }
+    
   
-
+    @IBAction func Diamonds(_ sender: Any) {
+        topLeft.text = "8"
+        bottomRight.text = "8"
+        centerLabel.text = "♦️"
+    }
+    
+    
+    @IBAction func Hearts(_ sender: Any) {
+        topLeft.text = "10"
+        bottomRight.text = "10"
+        centerLabel.text = "♥️"
+    }
+    
+    
+    
 }
