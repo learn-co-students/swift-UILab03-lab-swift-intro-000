0812bfe55ebfa955771d60cc886639910650c443
chadarutherford <bladz82@hotmail.com>
2018-11-09 16:40:55 -0500

    Completed Lab

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet var topSuitLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    @IBOutlet var bottomSuitLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,27 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
-
-  
-
+    @IBAction func fourClubsTapped(_ sender: UIButton) {
+        topSuitLabel.text = "♣️"
+        valueLabel.text = "4"
+        bottomSuitLabel.text = "♣️"
+    }
+    
+    @IBAction func threeSpadesTapped(_ sender: UIButton) {
+        topSuitLabel.text = "♠️"
+        valueLabel.text = "3"
+        bottomSuitLabel.text = "♠️"
+    }
+    
+    @IBAction func eightDiamondsTapped(_ sender: UIButton) {
+        topSuitLabel.text = "♦️"
+        valueLabel.text = "8"
+        bottomSuitLabel.text = "♦️"
+    }
+    
+    @IBAction func tenHeartsTapped(_ sender: UIButton) {
+        topSuitLabel.text = "♥️"
+        valueLabel.text = "10"
+        bottomSuitLabel.text = "♥️"
+    }
 }
