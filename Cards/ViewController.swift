c0669b659465965d3b65ba79d219f1b2e7dbe831
“dnomyar105” <dnomyar105@yahoo.com>
2018-12-03 12:44:24 -0500

    Complete problems

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+    @IBOutlet weak var suitTop: UILabel!
+    @IBOutlet weak var suitBottom: UILabel!
+    @IBOutlet weak var valueMiddle: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,30 @@ class ViewController: UIViewController {
     
     
     // TODO: IB actions and code to update UI
-
+    @IBAction func cardSelection(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            suitTop.text = "♣️"
+            suitBottom.text = "♣️"
+            valueMiddle.text = "4"
+        case 1:
+            suitTop.text = "♠️"
+            suitBottom.text = "♠️"
+            valueMiddle.text = "3"
+        case 2:
+            suitTop.text = "♦️"
+            suitBottom.text = "♦️"
+            valueMiddle.text = "8"
+        case 3:
+            suitTop.text = "♥️"
+            suitBottom.text = "♥️"
+            valueMiddle.text = "10"
+        default:
+            return
+        }
+        
+    }
+    
   
 
 }
