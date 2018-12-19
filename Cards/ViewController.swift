056102dd7afa9a2eb838e2dfb2d20b0fded55bd2
jidafi <jidafi@gmail.com>
2018-12-19 08:50:08 -0600

    Completed

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var middleLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    
-    
-    // TODO: IB actions and code to update UI
 
-  
-
+    @IBAction func club(_ sender: Any) {
+        suit(suitIn: "♣️", num: "4")
+    }
+    
+    
+    @IBAction func spade(_ sender: Any) {
+        suit(suitIn: "♠️", num: "3")
+    }
+    
+    
+    @IBAction func diamond(_ sender: Any) {
+        suit(suitIn: "♦️", num: "8")
+    }
+    
+    @IBAction func heart(_ sender: Any) {
+        suit(suitIn: "♥️", num: "10")
+    }
+    
+    func suit(suitIn: String, num: String) {
+        topLabel.text = suitIn
+        bottomLabel.text = suitIn
+        middleLabel.text = num
+        
+    }
 }
