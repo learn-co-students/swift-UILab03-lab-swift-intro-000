c8d8f8513e9efc07d5638199b9fdf60025bacace
pmaurelia <pmaurelia@gmail.com>
2019-02-19 22:16:04 -0500

    another working lab

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // TODO: Create IB outlets
+
+    @IBOutlet weak var topLeftSuit: UILabel!
+    @IBOutlet weak var centerSuit: UILabel!
+    @IBOutlet weak var bottomRightSuit: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        topLeftSuit.text = "?"
+        centerSuit.text = "?"
+        bottomRightSuit.text = "?"
+    }
+
+    @IBAction func clubsButton(_ sender: UIButton)
+    {
+        topLeftSuit.text = "♣️"
+        bottomRightSuit.text = "♣️"
+        centerSuit.text = "4"
+        print("4 of ♣️ pressed")
     }
     
+    @IBAction func spadesButton(_ sender: UIButton)
+    {
+        topLeftSuit.text = "♠️"
+        centerSuit.text = "3"
+        bottomRightSuit.text = "♣️"
+        print("3 of ♠️ selected")
+    }
     
+    @IBAction func diamondsButton(_ sender: UIButton)
+    {
+        topLeftSuit.text = "♦️"
+        centerSuit.text = "8"
+        bottomRightSuit.text = "♦️"
+        print("8 of ♦️ selected")
+    }
     
-    // TODO: IB actions and code to update UI
-
-  
-
+    @IBAction func heartsButton(_ sender: UIButton)
+    {
+        topLeftSuit.text = "♥️"
+        centerSuit.text = "10"
+        bottomRightSuit.text = "♥️"
+        print("10 of ♥️ selected")
+    }
 }
